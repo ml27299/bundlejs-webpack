65ebfbc8ce675bcfd3aab74bf1325c5f14ad445a
Macario Lara <ml27299@gmail.com>
2023-10-01 12:27:20 -0500

    update

---
+++ WatchForNewFilesPlugin.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const fs = require("fs");
+const FileWatcherPlugin = require("filewatcher-webpack-plugin");
+const getServerEntries = require("./getServerEntries");
+
+function run({ appBundleName, rootComponentPath }) {
+	let { bundlePaths, ssrBundlePaths } = getServerEntries({
+		appBundleName,
+		rootComponentPath,
+	});
+
+	bundlePaths = bundlePaths
+		.map((bundlePath) => path.dirname(bundlePath))
+		.map((bundlePath) => bundlePath.replace(path.resolve() + "/", ""));
+	ssrBundlePaths = ssrBundlePaths
+		.map((bundlePath) => path.dirname(bundlePath))
+		.map((bundlePath) => bundlePath.replace(path.resolve() + "/", ""));
+
+	const refreshBundles = (filePath) => {
+		const belongsTo = bundlePaths.filter(
+			(bundlePath) => filePath.indexOf(bundlePath) === 0
+		);
+		for (const bundlePath of belongsTo) {
+			const content = fs.readFileSync(
+				`${path.resolve(bundlePath)}/${appBundleName}`,
+				"utf8"
+			);
+			fs.writeFileSync(`${path.resolve(bundlePath)}/${appBundleName}`, content);
+		}
+	};
+
+	return new FileWatcherPlugin({
+		watchFileRegex: [path.resolve(`${rootComponentPath}/**`)],
+		onChangeCallback: () => {},
+		onAddCallback: (filePath) => {
+			refreshBundles(filePath);
+		},
+		onUnlinkCallback: (filePath) => {
+			refreshBundles(filePath);
+		},
+		ignoreInitial: true,
+		usePolling: false,
+		ignored: "/node_modules/",
+	});
+}
+
+module.exports = ({ appBundleName, rootComponentPath }) =>
+	run({ appBundleName, rootComponentPath });
